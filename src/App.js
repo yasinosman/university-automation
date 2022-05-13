935e5840336a64118c3f54954e3a30679f3b912c
yasinosman <yasinosman10@gmail.com>
2022-05-13 15:45:37 +0300

    chore: install react router to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Container } from "@mui/material";
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+
 function App() {
-	return <div className="App">Hello World</div>;
+	return (
+		<BrowserRouter>
+			<CssBaseline />
+			<Container>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="about" element={<AboutPage />} />
+				</Routes>
+			</Container>
+		</BrowserRouter>
+	);
 }
 
 export default App;
