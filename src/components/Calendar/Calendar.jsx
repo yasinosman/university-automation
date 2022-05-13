8aa37cf4f2d1c6c9afcd3e87efbb1cec70a571a5
hasibenur <hasibenur.karadag@gmail.com>
2022-05-13 20:52:18 +0300

    commit on takvim

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import isWeekend from 'date-fns/isWeekend';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+
+const Calendar = () =>{
+  const [value, setValue] = React.useState(new Date());
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDatePicker
+        orientation="landscape"
+        openTo="day"
+        value={value}
+        shouldDisableDate={isWeekend}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+    }
+    export default Calendar;
+
--- src/pages/Calendar/CalendarPage.jsx
+++ src/pages/Calendar/CalendarPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import Calendar from "../../components/Calendar";
+
 
 const CalendarPage = () => {
     return (
         <>
              <Navbar />
+             <Calendar />
         </>
+        
     );
 }
 
