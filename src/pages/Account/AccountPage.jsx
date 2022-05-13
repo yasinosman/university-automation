35550088191bb59cd3d227c35a80da9c54419e3f
yasinosman <yasinosman10@gmail.com>
2022-05-13 21:36:56 +0300

    feat(Account): add Change Password Form

--- src/pages/Account/AccountPage.jsx
+++ src/pages/Account/AccountPage.jsx
@@ -1,7 +1,8 @@
-import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import React from "react";
 import Navbar from "../../components/Navbar";
 import PageTitle from "../../components/PageTitle";
+import ChangePasswordForm from "./ChangePasswordForm";
 import EditProfileForm from "./EditProfileForm";
 
 const AccountPage = () => {
@@ -17,35 +18,7 @@ const AccountPage = () => {
 						<EditProfileForm />
 					</Grid>
 					<Grid item xs={12} md={6}>
-						<Box
-							component="form"
-							sx={{
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "space-between",
-								"& .MuiTextField-root": { marginBottom: 2 },
-							}}
-							noValidate
-							autoComplete="off"
-						>
-							<Typography variant="overline" display="block" gutterBottom>
-								Şifre Güncelleme
-							</Typography>
-							<TextField
-								id="current-password"
-								label="Mevcut Şifre"
-								type="password"
-								autoComplete="current-password"
-							/>
-							<TextField id="new-password" label="Yeni Şifre" variant="outlined" type="password" />
-							<TextField
-								id="confirm-new-password"
-								label="Yeni Şifre Tekrarı"
-								variant="outlined"
-								type="password"
-							/>
-							<Button variant="contained">Şifreyi Güncelle</Button>
-						</Box>
+						<ChangePasswordForm />
 					</Grid>
 				</Grid>
 			</Container>
