de8ee86040fa478e91bff5f4aae96bacbd2895a1
yasinosman <yasinosman10@gmail.com>
2022-05-14 14:06:37 +0300

    feat(Home): access individual news and event item in News Detail and Event Detail pages

--- src/pages/EventDetail/EventDetailPage.jsx
+++ src/pages/EventDetail/EventDetailPage.jsx
@@ -1,7 +1,24 @@
+import { Container } from "@mui/material";
 import React from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import PageTitle from "../../components/PageTitle";
+import events from "../Home/mock/events.json";
 
 const EventDetailPage = () => {
-	return <div>EventDetailPage</div>;
+	let { id } = useParams();
+
+	const event = React.useMemo(() => events.find((e) => parseInt(e.id) === parseInt(id)), [id]);
+
+	return (
+		<>
+			<Navbar />
+
+			<Container maxWidth="xl">
+				<PageTitle title={event.title} />
+			</Container>
+		</>
+	);
 };
 
 export default EventDetailPage;
--- src/pages/NewsDetail/NewsDetailPage.jsx
+++ src/pages/NewsDetail/NewsDetailPage.jsx
@@ -1,7 +1,24 @@
+import { Container } from "@mui/material";
 import React from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import PageTitle from "../../components/PageTitle";
+import news from "../Home/mock/news.json";
 
 const NewsDetailPage = () => {
-	return <div>NewsDetailPage</div>;
+	let { id } = useParams();
+
+	const newsItem = React.useMemo(() => news.find((n) => parseInt(n.id) === parseInt(id)), [id]);
+
+	return (
+		<>
+			<Navbar />
+
+			<Container maxWidth="xl">
+				<PageTitle title={newsItem.title} />
+			</Container>
+		</>
+	);
 };
 
 export default NewsDetailPage;
