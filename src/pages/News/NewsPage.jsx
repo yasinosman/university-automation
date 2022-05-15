8bb2e303babd619cbfe6ed6b234051e4a6b93a54
yasinosman <yasinosman10@gmail.com>
2022-05-15 12:36:27 +0300

    feat(News): navigate to news detail page from News page

--- src/pages/News/NewsPage.jsx
+++ src/pages/News/NewsPage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-import { Container,Avatar, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import { Container, Avatar, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 import Navbar from "../../components/Navbar";
 import PageTitle from "../../components/PageTitle";
 import news from "../Home/mock/news.json";
 import { LibraryBooks } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const NewsPage = () => {
+	const navigate = useNavigate();
+
+	const navigateToDetail = (newsItem) => {
+		navigate(`/news/${newsItem.id}`);
+	};
+
 	return (
 		<>
 			<Navbar />
@@ -15,50 +22,51 @@ const NewsPage = () => {
 				<PageTitle title="Tüm Haberler" />
 			</Container>
 			<Container maxWidth="xl">
-			<>
-			{news.length > 0 ? (
-				<List
-					sx={{
-						width: "100%",
-						bgcolor: "background.paper",
-					}}
-				>
-					{news.map((newsItem, index) => (
-						<React.Fragment key={newsItem.id}>
-							<ListItem alignItems="flex-start">
-								<ListItemAvatar>
-									<Avatar>
-										<LibraryBooks />
-									</Avatar>
-								</ListItemAvatar>
-								<ListItemText
-									primary={newsItem.title}
-									secondary={
-										<React.Fragment>
-											{newsItem.subtitle}
+				<>
+					{news.length > 0 ? (
+						<List
+							sx={{
+								width: "100%",
+								bgcolor: "background.paper",
+							}}
+						>
+							{news.map((newsItem, index) => (
+								<React.Fragment key={newsItem.id}>
+									<ListItem
+										alignItems="flex-start"
+										sx={{ "&:hover": { cursor: "pointer" } }}
+										onClick={() => navigateToDetail(newsItem)}
+									>
+										<ListItemAvatar>
+											<Avatar>
+												<LibraryBooks />
+											</Avatar>
+										</ListItemAvatar>
+										<ListItemText
+											primary={newsItem.title}
+											secondary={
+												<React.Fragment>
+													{newsItem.subtitle}
 
-											<Typography
-												variant="subtitle2"
-												component="span"
-												sx={{ mt: 1, display: "block" }}
-											>
-											 {newsItem.date}
-											</Typography>
-											
-										</React.Fragment>
-									}
-								/>
-							</ListItem>
-							{index !== news.length - 1 && (
-								<Divider variant="inset" component="li" />
-							)}
-						</React.Fragment>
-					))}
-				</List>
-			) : (
-				<Typography variant="body1">Yeni haber bulunmamaktadır</Typography>
-			)}
-		</>
+													<Typography
+														variant="subtitle2"
+														component="span"
+														sx={{ mt: 1, display: "block" }}
+													>
+														{newsItem.date}
+													</Typography>
+												</React.Fragment>
+											}
+										/>
+									</ListItem>
+									{index !== news.length - 1 && <Divider variant="inset" component="li" />}
+								</React.Fragment>
+							))}
+						</List>
+					) : (
+						<Typography variant="body1">Yeni haber bulunmamaktadır</Typography>
+					)}
+				</>
 			</Container>
 		</>
 	);
