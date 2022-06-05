755ed0a2d0054204a557acc311e7294bd7e0ea4a
yasinosman <yasinosman10@gmail.com>
2022-06-05 16:32:34 +0300

    feat(events): fetch events from backend services

--- src/pages/EventDetail/EventDetailPage.jsx
+++ src/pages/EventDetail/EventDetailPage.jsx
@@ -1,33 +1,40 @@
 import { Container, Divider, Typography } from "@mui/material";
 import React from "react";
 import { useParams } from "react-router-dom";
+import AlertPopup from "../../components/AlertPopup";
+import Loading from "../../components/Loading";
 import Navbar from "../../components/Navbar";
 import PageTitle from "../../components/PageTitle";
-import events from "../Home/mock/events.json";
+import { useFetch } from "../../hooks";
 
 const EventDetailPage = () => {
 	let { id } = useParams();
 
-	const event = React.useMemo(() => events.find((e) => parseInt(e.id) === parseInt(id)), [id]);
+	const { loading, error, value: event, setError } = useFetch(`/events/${id}`, {}, [id]);
 
 	return (
 		<>
 			<Navbar />
 
-			<Container maxWidth="xl">
-				<PageTitle title={event.title} />
-				<Typography variant="subtitle1" sx={{ mt: -2, mb: 1 }}>
-					{event.date}
-				</Typography>
-				<Typography variant="body1" sx={{ mb: 1, fontStyle: "italic" }}>
-					{event.subtitle}
-				</Typography>
-				<Divider />
-				<img src={event.imgURL} alt={event.imgAlt} style={{ marginTop: 20 }} />
-				<Typography variant="body1" sx={{ mb: 1, mt: 1 }}>
-					{event.content}
-				</Typography>
-			</Container>
+			{error && <AlertPopup error={error.message} handleClose={() => setError(false)} />}
+			<Loading loading={loading} />
+
+			{event && (
+				<Container maxWidth="xl">
+					<PageTitle title={event.title} />
+					<Typography variant="subtitle1" sx={{ mt: -2, mb: 1 }}>
+						{event.createdAt}
+					</Typography>
+					<Typography variant="body1" sx={{ mb: 1, fontStyle: "italic" }}>
+						{event.subtitle}
+					</Typography>
+					<Divider />
+					<img src={event.imgURL} alt={event.imgAlt} style={{ marginTop: 20 }} />
+					<Typography variant="body1" sx={{ mb: 1, mt: 1 }}>
+						{event.content}
+					</Typography>
+				</Container>
+			)}
 		</>
 	);
 };
--- src/pages/Events/EventsPage.jsx
+++ src/pages/Events/EventsPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import events from "../Home/mock/events.json";
 import Navbar from "../../components/Navbar";
 import PageTitle from "../../components/PageTitle";
 import { Avatar, Divider, Container, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 import { LibraryBooks } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
+import { useFetch } from "../../hooks";
+import AlertPopup from "../../components/AlertPopup";
+import Loading from "../../components/Loading";
 
 const EventsPage = () => {
 	const navigate = useNavigate();
@@ -12,6 +14,9 @@ const EventsPage = () => {
 	const navigateToEventDetail = (event) => {
 		navigate(`/events/${event.id}`);
 	};
+
+	const { loading, error, value: events, setError } = useFetch("/events");
+
 	return (
 		<>
 			<Navbar />
@@ -19,53 +24,61 @@ const EventsPage = () => {
 			<Container maxWidth="xl">
 				<PageTitle title="Tüm Etkinlikler" />
 			</Container>
-			<Container maxWidth="xl">
-				<>
-					{events.length > 0 ? (
-						<List
-							sx={{
-								width: "100%",
-								bgcolor: "background.paper",
-							}}
-						>
-							{events.map((event, index) => (
-								<React.Fragment key={event.id}>
-									<ListItem
-										alignItems="flex-start"
-										sx={{ "&:hover": { cursor: "pointer" } }}
-										onClick={() => navigateToEventDetail(event)}
-									>
-										<ListItemAvatar>
-											<Avatar>
-												<LibraryBooks />
-											</Avatar>
-										</ListItemAvatar>
-										<ListItemText
-											primary={event.title}
-											secondary={
-												<React.Fragment>
-													{event.subtitle}
 
-													<Typography
-														variant="subtitle2"
-														component="span"
-														sx={{ mt: 1, display: "block" }}
-													>
-														{event.date}
-													</Typography>
-												</React.Fragment>
-											}
-										/>
-									</ListItem>
-									{index !== events.length - 1 && <Divider variant="inset" component="li" />}
-								</React.Fragment>
-							))}
-						</List>
-					) : (
-						<Typography variant="body1">Yeni etkinlikler bulunmamaktadır</Typography>
-					)}
-				</>
-			</Container>
+			{error && <AlertPopup error={error.message} handleClose={() => setError(false)} />}
+			<Loading loading={loading} />
+
+			{events && (
+				<Container maxWidth="xl">
+					<>
+						{events.length > 0 ? (
+							<List
+								sx={{
+									width: "100%",
+									bgcolor: "background.paper",
+								}}
+							>
+								{events.map((event, index) => (
+									<React.Fragment key={event.id}>
+										<ListItem
+											alignItems="flex-start"
+											sx={{ "&:hover": { cursor: "pointer" } }}
+											onClick={() => navigateToEventDetail(event)}
+										>
+											<ListItemAvatar>
+												<Avatar>
+													<LibraryBooks />
+												</Avatar>
+											</ListItemAvatar>
+											<ListItemText
+												primary={event.title}
+												secondary={
+													<React.Fragment>
+														{event.subtitle}
+
+														<Typography
+															variant="subtitle2"
+															component="span"
+															sx={{ mt: 1, display: "block" }}
+														>
+															{event.createdAt}
+														</Typography>
+													</React.Fragment>
+												}
+											/>
+										</ListItem>
+										{index !== events.length - 1 && (
+											<Divider variant="inset" component="li" />
+										)}
+									</React.Fragment>
+								))}
+							</List>
+						) : (
+							<Typography variant="body1">Yeni etkinlikler bulunmamaktadır</Typography>
+						)}
+					</>
+				</Container>
+			)}
 		</>
 	);
 };
